71efead5595f59847bc34de3866c08ec23734e4a
kinchen <kinchen@cisco.com>
2014-02-18 15:24:39 +0800

    Test

--- EmailEngineDemo/src/java/com/cisco/webex/common/web/HttpUtilsTest.java
+++ EmailEngineDemo/src/java/com/cisco/webex/common/web/HttpUtilsTest.java
@@ -1,12 +1,17 @@
 package com.cisco.webex.common.web;
-/**   
+
+/**
  * @Title: HttpUtilsTest.java
  * @Package com.cisco.webex.common.web
  * @Description: TODO
  * @author King chen
- * @date Feb 18, 2014 2:16:58 PM    
- * @version V1.0   
+ * @date Feb 18, 2014 2:16:58 PM
+ * @version V1.0
  */
 public class HttpUtilsTest {
+    
+    public static String getHttpServerName() {
+        return "";
+    }
 
 }
